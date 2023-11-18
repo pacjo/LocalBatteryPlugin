896800bc9e10b0774e5689e40d2dd8b4a0b6cc98
pacjo <kamil.kroliczek2004@gmail.com>
2023-11-18 14:30:44 +0100

    working basic example

---
+++ app/src/main/java/nodomain/pacjo/smartspacer/localbattery/MainActivity.kt
@@ -0,0 +1,14 @@
+package nodomain.pacjo.smartspacer.localbattery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import nodomain.pacjo.smartspacer.localbattery.ui.theme.LocalBatteryPluginTheme
+
---
+++ app/src/main/java/nodomain/pacjo/smartspacer/localbattery/targets/LocalBatteryTarget.kt
@@ -0,0 +1,52 @@
+package nodomain.pacjo.smartspacer.localbattery.targets
+
+import android.content.ComponentName
+import android.graphics.drawable.Icon
+import com.kieronquinn.app.smartspacer.sdk.model.SmartspaceTarget
+import com.kieronquinn.app.smartspacer.sdk.model.uitemplatedata.Text
+import com.kieronquinn.app.smartspacer.sdk.provider.SmartspacerTargetProvider
+import com.kieronquinn.app.smartspacer.sdk.utils.TargetTemplate
+import nodomain.pacjo.smartspacer.localbattery.R
+
+class LocalBatteryTarget: SmartspacerTargetProvider() {
+
+    override fun getSmartspaceTargets(smartspacerId: String): List<SmartspaceTarget> {
+        return listOf(TargetTemplate.Basic(
+            id ="example_$smartspacerId",
+            componentName = ComponentName(provideContext(), LocalBatteryTarget::class.java),
+            title = Text("Hello World!"),
+            subtitle = Text("Example"),
+            icon = com.kieronquinn.app.smartspacer.sdk.model.uitemplatedata.Icon(
+                Icon.createWithResource(
+                    provideContext(),
+                    R.drawable.ic_launcher_foreground
+                )
+            )
+//            onClick = TapAction(
+//                intent = Intent(provideContext(), MainActivity::class.java)
+//            )
+        ).create())
+    }
+
+    override fun getConfig(smartspacerId: String?): Config {
+        return Config(
+            label = "Local Battery",
+            description = "Shows charging information",
+            icon = Icon.createWithResource(provideContext(), R.drawable.ic_launcher_foreground),    //TODO: change
+            // TODO: refreshPeriodMinutes = // Optional: How often the refresh broadcast for this Target should be called, in minutes (defaults to 0, never)
+            // TODO: compatibilityState = // Optional: A CompatibilityState object representing whether this Target is compatible with the device or not (defaults to always compatible)
+            // TODO: broadcastProvider = // Optional: The authority of a Broadcast Provider attached to this Target (see Broadcast Providers page)
+        )
+    }
+
+    override fun onDismiss(smartspacerId: String, targetId: String): Boolean {
+        //Handle dismissal of a Target, returning `true` when dismissed, or `false` if not dismissed
+        //TODO
+        return true
+    }
+
+//    override fun onProviderRemoved(smartspacerId: String) {
+//        //Handle removal of the Target (optional)
+//    }
+
+}
