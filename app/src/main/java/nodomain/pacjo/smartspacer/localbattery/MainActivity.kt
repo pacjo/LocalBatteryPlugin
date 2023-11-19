fe0b6961db545c1197417d41c940da4109b266d5
pacjo <kamil.kroliczek2004@gmail.com>
2023-11-19 16:03:32 +0100

    remove rest of theme code

--- app/src/main/java/nodomain/pacjo/smartspacer/localbattery/MainActivity.kt
+++ app/src/main/java/nodomain/pacjo/smartspacer/localbattery/MainActivity.kt
@@ -1,14 +1,7 @@
 package nodomain.pacjo.smartspacer.localbattery
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import nodomain.pacjo.smartspacer.localbattery.ui.theme.LocalBatteryPluginTheme
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
 
