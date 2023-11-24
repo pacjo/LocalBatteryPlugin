3619273a838ed713f80d8f8f8674e6e49d30e902
pacjo <kamil.kroliczek2004@gmail.com>
2023-11-24 06:56:41 +0100

    remove MainActivity

--- app/src/main/java/nodomain/pacjo/smartspacer/localbattery/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package nodomain.pacjo.smartspacer.localbattery
-
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.widget.Toast
-
